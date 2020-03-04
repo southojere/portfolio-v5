944d95693c4494e8a6d5dcee530ca60e3e0330e3
Jeremy <jeremy.southon@rocketlab.com.au>
2020-03-04 19:56:13 +1100

    Added google tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`
+  path: `.env.${process.env.NODE_ENV}`,
 })
 
 const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-  host: process.env.CONTENTFUL_HOST
+  host: process.env.CONTENTFUL_HOST,
 }
 
 const { spaceId, accessToken } = contentfulConfig
@@ -30,6 +30,12 @@ module.exports = {
     {
       resolve: 'gatsby-source-contentful',
       options: contentfulConfig,
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+       trackingId: 'UA-139866108-1',
+      },
+    },
   ],
 }
